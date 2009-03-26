aa456cce6bb14fa6c71ea352b064dba58c38e386
cubicreality <cubicreality@969c896a-e6fe-11dd-bd7b-b12b6590754f>
2009-03-26 10:29:42 +0000

    + Added License.
    git-svn-id: http://cubicexplorer.googlecode.com/svn/trunk@44 969c896a-e6fe-11dd-bd7b-b12b6590754f

--- Source/CE_DockInfo.pas
+++ Source/CE_DockInfo.pas
@@ -1,27 +1,23 @@
-{-----------------------------------------------------------------------------
-The contents of this file are subject to the Mozilla Public License
-Version 1.1 (the "License"); you may not use this file except in compliance
-with the License. You may obtain a copy of the License at
-http://www.mozilla.org/MPL/MPL-1.1.html
-
-Software distributed under the License is distributed on an "AS IS" basis,
-WITHOUT WARRANTY OF ANY KIND, either expressed or implied. See the License for
-the specific language governing rights and limitations under the License.
-
-The Original Code is: JvDockInfo.pas, released on 2003-12-31.
-
-The Initial Developer of the Original Code is luxiaoban.
-Portions created by luxiaoban are Copyright (C) 2002,2003 luxiaoban.
-All Rights Reserved.
-
-Contributor(s):
-
-You may retrieve the latest version of this file at the Project JEDI's JVCL home page,
-located at http://jvcl.sourceforge.net
-
-Known Issues:
------------------------------------------------------------------------------}
-// $Id: JvDockInfo.pas 10949 2006-09-29 12:37:30Z obones $
+//******************************************************************************
+//  CubicExplorer                                                                             
+//  Version: 0.90                                                                             
+//                                                                                            
+//  The contents of this file are subject to the Mozilla Public License                       
+//  Version 1.1 (the "License"); you may not use this file except in                          
+//  compliance with the License. You may obtain a copy of the License at                      
+//  http://www.mozilla.org/MPL/                                                               
+//                                                                                            
+//  Software distributed under the License is distributed on an "AS IS"
+//  basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+//  License for the specific language governing rights and limitations                        
+//  under the License.                                                                        
+//                                                                                            
+//  The Original Code is JvDockInfo.pas, released on 2003-12-31.                                                        
+//                                                                                            
+//  The Initial Developer of the Original Code is luxiaoban.  
+//  Portions created by luxiaoban Copyright (C) 2002,2003 luxiaoban. All Rights Reserved. 
+//                                                                                            
+//******************************************************************************
 
 unit CE_DockInfo;
 
